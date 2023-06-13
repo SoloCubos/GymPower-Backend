3a23e79839b2b20102de4ca8470fed9dfa0aa9bd
SoloCubos <solanofernando35@gmail.com>
2023-06-13 14:43:15 -0500

    Creación Proyecto

---
+++ src/main/java/com/gympower/web/GymPowerApplication.java
@@ -0,0 +1,13 @@
+package com.gympower.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymPowerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymPowerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gympower/web/GymPowerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gympower.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymPowerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
